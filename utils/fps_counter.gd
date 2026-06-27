50baf73fac8a83a6e82cd69a97fac6b05cc0b12c
Mnzo <81849467+Mnzo@users.noreply.github.com>
2026-06-27 18:08:48 +0200

    DA elements

--- utils/fps_counter.gd
+++ utils/fps_counter.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Label
 
 @onready var label = $Label
 
@@ -13,4 +13,4 @@ func _process(_delta: float) -> void:
 	var main_thread = Performance.get_monitor(Performance.TIME_PROCESS) * 1000.0
 	var cpu_time = RenderingServer.viewport_get_measured_render_time_cpu(root_viewport_rid)
 	var gpu_time = RenderingServer.viewport_get_measured_render_time_gpu(root_viewport_rid)
-	label.text = "FPS: %d\nGame: %.2f ms\nCPU: %.2f ms\nGPU: %.2f ms" % [fps, main_thread, cpu_time, gpu_time]
+	text = "FPS: %d\nGame: %.2f ms\nCPU: %.2f ms\nGPU: %.2f ms" % [fps, main_thread, cpu_time, gpu_time]
